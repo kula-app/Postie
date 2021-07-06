0b463e18196bf0fd0ec48cd84d359a9859a9c5f4
Philip Niedertscheider <dev@philpri.me>
2021-07-06 13:47:46 +0200

    Added callback method and tests

--- Sources/Postie/Networking/URLSessionProvider.swift
+++ Sources/Postie/Networking/URLSessionProvider.swift
@@ -5,6 +5,7 @@ import Combine
 public protocol URLSessionProvider {
 
     func send(urlRequest request: URLRequest) -> AnyPublisher<URLSession.DataTaskPublisher.Output, URLSession.DataTaskPublisher.Failure>
+    func dataTask(with request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void)
 
 }
 
@@ -13,5 +14,10 @@ extension URLSession: URLSessionProvider {
 
     public func send(urlRequest request: URLRequest) -> AnyPublisher<URLSession.DataTaskPublisher.Output, URLSession.DataTaskPublisher.Failure> {
         self.dataTaskPublisher(for: request).eraseToAnyPublisher()
+    }
+
+    public func dataTask(with request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let task = self.dataTask(with: request, completionHandler: completion)
+        task.resume()
     }
 }
