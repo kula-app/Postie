bf545373c547f17f0fb49c5de1b27df50dc50ee7
Philip Niedertscheider <dev@philpri.me>
2021-07-14 00:03:30 +0200

    Added better error logging to LoggingJSONDecoder

--- Sources/Postie/Utils/LoggingJSONDecoder.swift
+++ Sources/Postie/Utils/LoggingJSONDecoder.swift
@@ -7,7 +7,7 @@ class LoggingJSONDecoder: JSONDecoder {
         do {
             return try super.decode(type, from: data)
         } catch {
-            os_log("Failed to decode JSON: %@\nReason: %@", type: .error, String(data: data, encoding: .utf8) ?? "nil", error.localizedDescription)
+            os_log("Failed to decode JSON: %@\nReason: %@\nDetails: %@", type: .error, String(data: data, encoding: .utf8) ?? "nil", error.localizedDescription, String(describing: error))
             throw error
         }
     }
