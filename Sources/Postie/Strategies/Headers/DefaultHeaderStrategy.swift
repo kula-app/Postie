bd7b5241e0335f13677777c2ab9b1e792a44ffee
Antoine Barré <developer.projects@pomanks.com>
2021-12-10 15:17:10 +0100

    Added convenience initializer, Bool and Double and ACL of QueryItem (#12)

--- Sources/Postie/Strategies/Headers/DefaultHeaderStrategy.swift
+++ Sources/Postie/Strategies/Headers/DefaultHeaderStrategy.swift
@@ -2,23 +2,23 @@ import Foundation
 
 public struct DefaultHeaderStrategy: ResponseHeaderDecodingStrategy {
 
-    public typealias RawValue = String
+  public typealias RawValue = String
 
-    public static func decode(decoder: Decoder) throws -> RawValue {
-        guard let key = decoder.codingPath.last?.stringValue else {
-            throw ResponseHeaderDecodingError.missingCodingKey
-        }
-        // Check if the decoder is response decoder, otherwise fall back to default decoding logic
-        guard let responseDecoding = decoder as? ResponseDecoding else {
-            return try RawValue(from: decoder)
-        }
-        // Transform dash separator to camelCase
-        guard let value = responseDecoding.valueForHeaderCaseInsensitive(key) else {
-            throw DecodingError.valueNotFound(RawValue.self, DecodingError.Context(
-                codingPath: decoder.codingPath,
-                debugDescription: "Missing value for case-insensitive header key: \(key)"
-            ))
-        }
-        return value
+  public static func decode(decoder: Decoder) throws -> RawValue {
+    guard let key = decoder.codingPath.last?.stringValue else {
+      throw ResponseHeaderDecodingError.missingCodingKey
     }
+    // Check if the decoder is response decoder, otherwise fall back to default decoding logic
+    guard let responseDecoding = decoder as? ResponseDecoding else {
+      return try RawValue(from: decoder)
+    }
+    // Transform dash separator to camelCase
+    guard let value: RawValue = responseDecoding.valueForHeaderCaseInsensitive(key) else {
+      throw DecodingError.valueNotFound(RawValue.self, DecodingError.Context(
+        codingPath: decoder.codingPath,
+        debugDescription: "Missing value for case-insensitive header key: \(key)"
+      ))
+    }
+    return value
+  }
 }
