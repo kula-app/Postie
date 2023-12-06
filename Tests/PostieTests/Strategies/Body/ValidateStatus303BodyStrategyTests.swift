380941868b18157d75ccaa02e4f399ced34ee127
Philip Niedertscheider <phil@kula.app>
2023-12-06 11:21:52 +0100

    tests: added missing test for validate status 303 decoding strategy (#39)

---
+++ Tests/PostieTests/Strategies/Body/ValidateStatus303BodyStrategyTests.swift
@@ -0,0 +1,20 @@
+@testable import Postie
+import XCTest
+
+class ValidateStatus303BodyStrategyTests: XCTestCase {
+    func testAllowsEmptyContent_statusIs303_shouldBeTrue() {
+        XCTAssertTrue(ValidateStatus303BodyStrategy.allowsEmptyContent(for: 303))
+    }
+
+    func testAllowsEmptyContent_statusIsNot303_shouldBeTrue() {
+        XCTAssertTrue(ValidateStatus303BodyStrategy.allowsEmptyContent(for: 999))
+    }
+
+    func testValidate_statusIs303_shouldBeTrue() {
+        XCTAssertTrue(ValidateStatus303BodyStrategy.validate(statusCode: 303))
+    }
+
+    func testValidate_statusIsNot303_shouldBeFalse() {
+        XCTAssertFalse(ValidateStatus303BodyStrategy.validate(statusCode: 999))
+    }
+}
